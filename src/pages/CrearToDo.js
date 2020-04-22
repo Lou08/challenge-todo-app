28ef20d181bec708c44c5bfbf77f1447fd87c067
Lou08 <l.casals86@gmail.com>
2020-04-22 13:33:17 +0200

    envio

--- src/pages/CrearToDo.js
+++ src/pages/CrearToDo.js
@@ -1,71 +1,87 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import todoservice from '../lib/todo'
 
-class CrearToDo extends Component{
-
-    state = {
-        title:"",
-        body:"",
+class CrearToDo extends Component {
+    constructor(props){
+        super(props)
+    
+    this.state = {
+        listToDo: [],
+        title: "",
+        body: "",
     };
+}
     handleChange = (event) => {
         const { name, value } = event.target;
         console.log('event.target :>> ', event.target);
         this.setState({ [name]: value });
-      };
-    
-      getInfo = () => {
+    };
+
+    getInfo = () => {
         todoservice
-          .getToDo()
-          .then((listadoEventos) => {
-            this.setState({ listadoEventos: listadoEventos.data });
-          })
-          .catch((err) =>
-            console.log("Este error pertenece a pagina ocio.js:", err)
-          );
-      };
-    
-      handleFormSubmit = async (event) => {
+            .getToDo()
+            .then((listToDo) => {
+                this.setState({ listToDo: listToDo.data });
+                console.log('listado',listToDo)
+            })
+            .catch((err) =>
+                console.log("Este error pertenece a pagina ocio.js:", err)
+            );
+    };
+
+    handleFormSubmit = async (event) => {
         event.preventDefault();
         const { title, body } = this.state;
         todoservice.createToDo({ title, body });
         await this.getInfo();
         this.props.history.push("/todos");
-      };
-      componentDidMount() {
+    };
+    componentDidMount() {
         this.getInfo();
-      }
+    }
 
-    render(){
-        const {title,description}=this.state;
+    render() {
+        const { title, description } = this.state;
+        const listadoToDo = this.state.listToDo.map((detalle) => {
+            return (
+                <div>
+                    <h1>{detalle.title}</h1>
+                    <h3>{detalle.description}</h3>
+                </div>
 
-        return(
+            );
+        });
+
+        return (
             <>
-            <div>
-            <form onSubmit={this.handleFormSubmit}>
-            <label>Title</label>
-            <input
-              type="text"
-              name="title"
-              placeholder="Title to do"
-              value={title}
-              onChange={ e => this.handleChange(e)}
-            />
+                <div>
 
-            <label>Description:</label>
-            <input
-              type="text"
-              name="description"
-              placeholder="Enter description"
-              value={description}
-              onChange={ e => this.handleChange(e)}
-            />
-            <input
-              type="submit"
-              value="Crear Evento"
-              className="buttontosubmit"
-            />
-          </form>
-            </div>
+                    {listadoToDo}
+                    <form onSubmit={this.handleFormSubmit}>
+                        <label>Title</label>
+                        <input
+                            type="text"
+                            name="title"
+                            placeholder="Title to do"
+                            value={title}
+                            onChange={e => this.handleChange(e)}
+                        />
+
+                        <label>Description:</label>
+                        <input
+                            type="text"
+                            name="description"
+                            placeholder="Enter description"
+                            value={description}
+                            onChange={e => this.handleChange(e)}
+                        />
+                        <input
+                            type="submit"
+                            value="Crear Evento"
+                            className="buttontosubmit"
+                        />
+                    </form>
+                </div>
 
             </>
 
